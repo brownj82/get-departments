8a612dc37e04c63061ce0396c1c5ddda278b2966
jared <188791753+brownj82@users.noreply.github.com>
2025-03-06 11:24:44 -0500

    Get rid of the GUI because I hate it

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
-# get-departments
+# Instructions
 
-Tool to pull department numbers based on MBU
+## Please ensure the following:
+- Most up-to-date subdepartment list is downloaded to this directory and named 'subdepartment_list.xlsx'
+- Python is installed and added to PATH
+- Git is installed
+- Virtual environment is created
+- All python required python libraries are installed from 'requirements.txt'
 
-## Roadmap
-- ~~Initial build~~
-- Simple GUI build
-    - Filesystem
-    - Inputs and submit button
-    - Copy output button
-- Make more interactive
-- Compile for use by entire team
+## Instructions:
+- Navigate to the the program directory
+- Activate Virtual Environment
+- To run in Jupyter Notebook:
+    - Enter 'jupyter notebook' on command line and select 'main.ipynb'
+- To run on command line:
+    - Enter 'python main.py'
+- Enter the desired MBU number. The department list will show on screen and automatically be copied to your system clipboard to paste in its entirety
+
+
--- main.py
+++ main.py
@@ -1,41 +1,19 @@
 from openpyxl import load_workbook
-import tkinter as tk
-from tkinter import ttk
 import pyperclip
 
-# Get departments
-def getDepartment():
-    workbook = load_workbook(filename = './subdepartment_list.xlsx')
-    sheet = workbook.active
-    departments = []
-    mbu_value = mbu.get()
-    for row in sheet.iter_rows(min_row = 4, min_col = 5, max_col = 7,values_only = True):
-        if row[0] == mbu_value:
-            departments.append(row[2])
-    result = ', '.join(map(str, set(departments)))
-    pyperclip.copy(result)
-    department_label.configure(text = f'The following departments were copied to the clipboard:\n{result}')
+file_location = './subdepartment_list.xlsx'
 
-# Create window
-root = tk.Tk()
-root.title('MBU to Department')
-root.geometry('500x300+50+50')
+workbook = load_workbook(filename = file_location)
+sheet = workbook.active
 
-# Create frame
-content = ttk.Frame(root)
-content.grid(column = 0, row = 0)
+departments = []
+mbu = int(input('mbu: '))
 
-# Create note frame
-note = ttk.Label(content, text = 'Please ensure you have downloaded the University Subdepartment List and saved it as a .xlsx file in the same directory as this application.', wraplength = 400).grid(column = 0, row = 0, columnspan = 3)
+for row in sheet.iter_rows(min_row=4, min_col=5, max_col=7,values_only=True):
+    if row[0] == mbu:
+        departments.append(row[2])
+        
+result = ', '.join(map(str, set(departments)))
+pyperclip.copy(result)
 
-# Create mbu frame
-mbu = tk.IntVar()
-mbu_label = ttk.Label(content, text = 'MBU #').grid(column = 0, row = 1, sticky = 'w')
-mbu_entry = ttk.Entry(content, textvariable = mbu).grid(column = 1, row = 1, columnspan = 2, sticky = 'w')
-mbu_search_button = ttk.Button(content, text = 'Search', command = getDepartment).grid(column = 3, row = 1, sticky = 'w')
-
-# Create department frame
-department_label = ttk.Label(content, text = '', wraplength = 400)
-department_label.grid(column = 0, row = 2, columnspan = 5, rowspan = 5, sticky = 'W')
-
-root.mainloop()
+print(f'\nThe following has been clipped to the system clipboard:\n{result}')
